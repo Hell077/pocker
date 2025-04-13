d2b788fe165535d9fd7949b98937645065f6e4f5
Alexey Ovchinnikov <alexey.ovchinnikov.dev@gmail.com>
2025-04-13 13:11:38 +0500

    add temporal hostport in client.go

--- apps/Backend/internal/temporal/client.go
+++ apps/Backend/internal/temporal/client.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"go.temporal.io/sdk/client"
 	"go.uber.org/zap"
+	"os"
 )
 
 func NewClient(logger *zap.Logger) client.Client {
-	c, err := client.Dial(client.Options{})
+	c, err := client.Dial(client.Options{
+		HostPort: os.Getenv("TEMPORAL_ADDRESS"),
+	})
 	if err != nil {
 		logger.Error(fmt.Sprintf("Unable to create Temporal client: %v", err))
 	}
