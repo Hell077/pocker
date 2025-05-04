1d5e2cb283b1a05b77092a48e36db273a32f6453
Prokhrenko Roman <rprohorenko@mail.ru>
2025-05-04 22:05:04 +0500

    shit2

--- apps/frontendv2/src/root/App.tsx
+++ apps/frontendv2/src/root/App.tsx
@@ -3,8 +3,9 @@ import { Routes, Route } from 'react-router-dom'
 import MainPage from '@/pages/main/main.tsx'
 import ProfilePage from '@/pages/Profile/Profile'
 import LeaderBoardPage from '@/pages/LeaderBoard/LeaderBoard'
-import LobbyPage from '@/pages/Lobby/Lobby' // 👈 импорт новой страницы
+import LobbyPage from '@/pages/Lobby/Lobby'
 import FortuneWheel from '@/pages/FortuneWheel/FortuneWheel'
+import PokerTablePage from '@/pages/PokerTable/Table.tsx'
 
 const App: FC = () => {
     return (
@@ -13,8 +14,9 @@ const App: FC = () => {
                 <Route path="/" element={<MainPage />} />
                 <Route path="/profile" element={<ProfilePage />} />
                 <Route path="/leaderboard" element={<LeaderBoardPage />} />
-                <Route path="/lobby" element={<LobbyPage />} /> {/* 👈 добавлен маршрут */}
+                <Route path="/lobby" element={<LobbyPage />} />
                 <Route path="/FortuneWheel" element={<FortuneWheel />} />
+                <Route path="/table/:id" element={<PokerTablePage />} />
             </Routes>
         </div>
     )
