afc31b4f7167fcd4c880e1886eab8831b1e8c0a8
Alexey Ovchinnikov <147360086+Hell077@users.noreply.github.com>
2025-05-21 12:56:18 +0500

    Update api.ts

--- apps/frontendv2/src/env/api.ts
+++ apps/frontendv2/src/env/api.ts
@@ -1,4 +1,4 @@
-// export const API_URL = window.ENV?.VITE_API_URL ?? 'http://localhost:3000/api';
-// export const WS_URL = window.ENV?.VITE_WS_URL ?? 'ws://localhost:3000/api/connection/ws';
-export const API_URL =  "http://localhost:3000/api" ;
-export const WS_URL = "ws://localhost:3000/api/connection/ws"
+ export const API_URL = window.ENV?.VITE_API_URL ?? 'http://localhost:3000/api';
+ export const WS_URL = window.ENV?.VITE_WS_URL ?? 'ws://localhost:3000/api/connection/ws';
+//export const API_URL =  "http://localhost:3000/api" ;
+//export const WS_URL = "ws://localhost:3000/api/connection/ws"
